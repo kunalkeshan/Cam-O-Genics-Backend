4e5b517bf3d22015fa528b9e89cb4bf57c7527fd
Kunal Keshan <kunalkeshan12@gmail.com>
2022-08-26 11:21:08 +0530

    router configuration

---
+++ routers/events.js
@@ -0,0 +1,9 @@
+/**
+ * Events Routers
+ */
+
+// Dependencies
+const Router = require('express').Router();
+
+// Exporting Router
+module.exports = Router;
--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,13 @@
 
 // Dependencies
 const Router = require('express').Router();
+const authRouter = require('./auth');
+const userRouter = require('./user');
+const eventsRouter = require('./events');
+
+Router.use('/api/auth', authRouter);
+Router.use('/api/user', userRouter);
+Router.use('/api/events', eventsRouter);
 
 // Exporting Router
 module.exports = Router;
