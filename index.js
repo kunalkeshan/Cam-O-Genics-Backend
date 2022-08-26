206fbc6c13b1a6abec9a011c26de1ab88f994ed8
Kunal Keshan <kunalkeshan12@gmail.com>
2022-08-26 16:20:38 +0530

    using cors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 const appRouter = require('./routers');
 const { ApiError } = require('./utils/custom');
 const errorHandler = require('./middlewares/apiError');
@@ -19,6 +20,7 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Application Router
