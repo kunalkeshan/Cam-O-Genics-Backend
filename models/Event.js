e407f7c905c83f94f4b7453311efc6e35e307e71
Kunal Keshan <kunalkeshan12@gmail.com>
2022-08-26 09:17:43 +0530

    event schema updated

--- models/Event.js
+++ models/Event.js
@@ -6,7 +6,44 @@
 const {Schema, model} = require('mongoose');
 
 // Event Schema
-const EventSchema = new Schema({});
+const EventSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    managers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    mode: {
+        type: String,
+        enum: ['ONLINE', 'OFFLINE'],
+        required: true,
+        
+    },
+    location: String,
+    link: String,
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    }
+}, {
+    timestamps: true,
+    skipVersioning: true,
+});
 
 // Event Model
 const Event = model('Event', EventSchema);
