f6f111b9e2328639a44ac49c886b34c2ed699bbd
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-12 00:18:09 +0530

    user controllers updated

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,3 @@
-/* eslint-disable consistent-return */
-/* eslint-disable no-empty */
-/* eslint-disable no-unused-vars */ // Remove Later
 /**
  * User Controllers
  */
@@ -30,32 +27,62 @@ UserController.updateName = async (req, res, next) => {
 };
 
 UserController.updatePhone = async (req, res, next) => {
+    const { phone } = req.body;
+    const { user } = req;
     try {
+        if (phone.length < 0) throw new ApiError({ message: 'user/phone-cannot-be-empty', statusCode: 400 });
+        await User.updateOne({ id: user.id }, { phone });
 
+        return res.status(200).json({
+            message: 'user/phone-updated',
+            data: { phone },
+            success: true,
+        });
     } catch (error) {
         return next(error);
     }
 };
 
 UserController.updateAddress = async (req, res, next) => {
+    const { address } = req.body;
+    const { user } = req;
     try {
+        if (address.length < 0) throw new ApiError({ message: 'user/address-cannot-be-empty', statusCode: 400 });
+        await User.updateOne({ id: user.id }, { address });
 
+        return res.status(200).json({
+            message: 'user/address-updated',
+            data: { address },
+            success: true,
+        });
     } catch (error) {
         return next(error);
     }
 };
 
 UserController.updatePassword = async (req, res, next) => {
+    const { password } = req.body;
+    const { user } = req;
     try {
+        if (password.length < 0) throw new ApiError({ message: 'user/password-cannot-be-empty', statusCode: 400 });
+        user.password = password;
+        await user.save();
 
+        return res.status(200).json({
+            message: 'user/password-updated',
+            success: true,
+        });
     } catch (error) {
         return next(error);
     }
 };
 
 UserController.updateCommunityIdentities = async (req, res, next) => {
+    const { communityIdentities } = req.body;
+    const { user } = req;
     try {
-
+        user.communityIdentities = communityIdentities;
+        return res.status(200).json({});
     } catch (error) {
         return next(error);
     }
