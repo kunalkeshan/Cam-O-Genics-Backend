e3a2efd5943b7df58df3f3389f115b7f781775d6
Kunal Keshan <kunalkeshan12@gmail.com>
2022-09-14 12:55:53 +0530

    updated notifications schema

--- models/Notifications.js
+++ models/Notifications.js
@@ -6,10 +6,59 @@
 const { Schema, model } = require('mongoose');
 
 // Notifications Schema
-const NotificationsSchema = new Schema({});
+const NotificationsSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    redirectUrl: {
+        type: String,
+        required: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    updatedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    category: {
+        type: String,
+        enum: ['EVENT', 'UPDATE'],
+        required: true,
+    },
+}, {
+    timestamps: true,
+    skipVersioning: true,
+});
+
+// Intermediate Notifications Schema
+// Connects the user with their read notifications
+const UserNotificationsSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    read: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Notifications',
+    }],
+}, {
+    timestamps: false,
+    skipVersioning: true,
+});
 
 // Notifications Model
 const Notifications = model('Notifications', NotificationsSchema);
+const UserNotifications = model('UserNotifications', UserNotificationsSchema);
 
 // Exporting Notifications
-module.exports = Notifications;
+module.exports = { Notifications, UserNotifications };
