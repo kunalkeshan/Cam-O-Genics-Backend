3e01410442c451f6e4582dc412f402c505dd9e88
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-14 20:57:34 +0530

    creating events features

--- controllers/events.js
+++ controllers/events.js
@@ -7,5 +7,11 @@
 // Events Controller Container
 const EventsController = {};
 
+// EventsController.createNewEvent = async (req, res, next) => { };
+
+// EventsController.updateEvent = async (req, res, next) => { };
+
+// EventsController.deleteEvent = async (req, res, next) => { };
+
 // Exporting Controller
 module.exports = EventsController;
