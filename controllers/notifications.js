238298707470203011c6e5bf5aa8f663d7cb5b02
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-14 20:56:04 +0530

    adding future routes

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -7,5 +7,15 @@
 // Notifications Controller Container
 const NotificationsController = {};
 
+// NotificationsController.createNewNotification = async (req, res, next) => { };
+
+// NotificationsController.updateNotification = async (req, res, next) => { };
+
+// NotificationsController.deleteNotification = async (req, res, next) => { };
+
+// NotificationsController.getUserNotifications = async (req, res, next) => { };
+
+// NotificationsController.markNotificationAsRead = async (req, res, next) => { };
+
 // Exporting Controller
 module.exports = NotificationsController;
