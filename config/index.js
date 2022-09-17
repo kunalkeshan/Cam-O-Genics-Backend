86b77af75aa1436471bed1aa293f7242cc019cad
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-17 07:58:55 +0530

    ramapuram register num regex added

--- config/index.js
+++ config/index.js
@@ -9,11 +9,18 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const DB_URL = !isProduction ? process.env.DB_URL_DEV : process.env.DB_URL_PROD;
 
+/*
+    RA(Year)2(Discipline)2(Course)4(Campus)2(Roll no)3
+    RA2011004010051
+    RA20110320 2  0  0  1  8
+    0123456789 10 11 12 13 14
+ */
+
 const APP_REGEX = {
     COLLEGE_EMAIL: /^(([a-z]{2}[0-9]{4})(@srmist\.edu\.in))$/,
     COGID: /^(COG)[0-9]{6}$/,
     PASSWORD: /^[\w@./#&+-]{6,}$/,
-    RAMAPURAM_ENGINEERING_REGISTER_NO: '',
+    RAMAPURAM_ENGINEERING_REGISTER_NO: /^RA([0-9]{2})11([0-9]{4})20([0-9]{3})$/,
     FORGOT_PASSWORD_OTP_FORMAT: /^[0-9]{4,}$/,
 };
 
