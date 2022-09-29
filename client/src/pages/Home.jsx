7607b670169ff6a5e01017317664de55e39dc81e
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-30 01:30:36 +0530

    feat: open admin panel with period key

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,27 @@
  * Home Page
  */
 
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import Navbar from '../components/layouts/Navbar'
 import Footer from '../components/layouts/Footer'
 
+
 const Home = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        window.addEventListener('keyup', (event) => {
+            // . - Period to open up admin app
+            if (event.key === '.') {
+                navigate('/admin');
+            };
+            console.log(event)
+        });
+        return () => window.removeEventListener('keyup', () => null)
+    }, [navigate]);
+
     return (
         <div>
             <Navbar />
