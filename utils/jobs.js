dc981ee4161bde65eb756009339e37501d301d40
kunalkeshan <kunalkeshan12@gmail.com>
2022-09-29 13:00:36 +0530

    feat: job to clear audits

--- utils/jobs.js
+++ utils/jobs.js
@@ -4,9 +4,27 @@
 
 // Dependencies
 const scheduler = require('node-schedule');
+const Audit = require('../models/Audit');
 const { clearExpiredOtps } = require('./otp');
 
 const EVERY_TWO_MINS = '*/2 * * * *';
+const EVERY_DAY = '0 0 * * *';
 
-// Check for expired otps every two mins
+const clearAuditLogs = async () => {
+    try {
+        const currentDate = new Date();
+        const audits = await Audit.find({}).limit(200);
+        audits.forEach((audit) => {
+            const auditCreatedDate = new Date(audit.createdAt);
+            const numberOfDays = (currentDate.getTime() - auditCreatedDate().getTime()) / (1000 * 3600 * 24);
+            if (numberOfDays > 30) {
+                audit.delete();
+            }
+        });
+        // eslint-disable-next-line no-empty
+    } catch (error) { }
+};
+
 scheduler.scheduleJob('Delete expired otp files', EVERY_TWO_MINS, clearExpiredOtps);
+
+scheduler.scheduleJob('Clear old audits, over a month old', EVERY_DAY, clearAuditLogs);
